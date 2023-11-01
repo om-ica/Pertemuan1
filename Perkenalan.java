be1462d2685a936b415a4683d68d42497d2b55e6
om-ica <irsacahaya17@gmail.com>
2023-11-01 20:10:03 +0700

    memperbarui

--- Perkenalan.java
+++ Perkenalan.java
@@ -2,6 +2,8 @@ public class perkenalan {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
-        System.out.println("Perkenalkan nama saya ");
+        System.out.println("Perkenalkan nama saya Irsa Cahaya Widodo");
+        System.out.println("Saya adalah mahasiswa Polinema");
+        System.out.println("Saya berasal dari jurusan Teknlogi Informasi");
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-mencoba
+Halo nama saya Irsa Cahaya Widodo
+Saya adalah mahasiswa Polinema
+Saya bersal dari jurusan Teknologi Informasi
