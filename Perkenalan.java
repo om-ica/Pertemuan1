d9a120f8873a89a75a3716b5a477bc8997f24533
om-ica <irsacahaya17@gmail.com>
2023-11-01 20:04:47 +0700

    Menambahkan perkenlan

--- Perkenalan.java
+++ Perkenalan.java
@@ -2,5 +2,6 @@ public class perkenalan {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
+        System.out.println("Perkenalkan nama saya ");
     }
 }
